c8757544250bcf3f2edb138d187f00118941f078
Chris Wang <heyitschrisw@gmail.com>
2021-04-02 11:52:13 -0700

    Updated AuthContext to utilize firebaseConfig

--- client/src/firebaseConfig.jsx
+++ client/src/firebaseConfig.jsx
@@ -10,5 +10,5 @@ const firebaseConfig = firebase.intializeApp({
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 })
 
-export default firebaseConfig.auth;
+export const auth = firebaseConfig.auth();
 export default firebaseConfig;
