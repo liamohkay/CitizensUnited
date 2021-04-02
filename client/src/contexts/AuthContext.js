6c4cadf2b41c18b6384b1dea867ee4089304e5c6
Chris Wang <heyitschrisw@gmail.com>
2021-04-02 15:18:36 -0700

    Updated authContext to include login

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { auth } from '../firebase';
+import React, { useState, useEffect } from 'react';
+import { auth } from '../firebaseConfig';
 
 const AuthContext = React.createContext();
 
@@ -8,17 +8,24 @@ export function useAuth() {
 }
 
 export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState()
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
 
   function signup(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password);
+    return auth.createUserWithEmailAndPassword(email, password)
+  }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password)
   }
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
+      setLoading(false);
     })
-    return unsubscribe;
+
+    return unsubscribe
   }, [])
 
   const value = {
@@ -30,5 +37,5 @@ export function AuthProvider({ children }) {
     <AuthContext.Provider value={value}>
       {children}
     </AuthContext.Provider>
-  );
+  )
 }
