a749bc01f8b7355472966386acc90fe849b12cfa
weillytong <weillydtong@gmail.com>
2021-04-02 12:58:41 -0700

    Completed initial SignUp component. Need to connect with firebase

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,13 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Form, Button, Card } from 'react-bootstrap';
 
 const SignUp = () => {
-  const [firstName, setFirstName] = useState();
-  const [lastName, setLastName] = useState();
-  const [phone, setPhone] = useState();
-  const [address, setAddress] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // const firstNameRef = useRef(() => console.log(firstNameRef));
+
+  const changeFirstName = (e) => {
+    setFirstName(e.target.value);
+    console.log(firstName);
+  }
+
+  const changeLastName = (e) => {
+    setLastName(e.target.value);
+    console.log(lastName);
+  }
+
+  const changePhone = (e) => {
+    setPhone(e.target.value);
+    console.log(phone);
+  }
+
+  const changeAddress = (e) => {
+    setAddress(e.target.value);
+    console.log(address);
+  }
+
+  const changeEmail = (e) => {
+    setEmail(e.target.value);
+    console.log(email);
+  }
+
+  const changePassword = (e) => {
+    setPassword(e.target.value);
+    console.log(password);
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    // INSERT POST REQUEST
+  }
+
+
 
   return (
     <div id="signUp-container">
@@ -17,86 +55,70 @@ const SignUp = () => {
           <Form>
             <Form.Group id="signUpFirstName">
               <Form.Label> First Name </Form.Label>
-              <Form.Control type="firstName" required/>
+              <Form.Control
+                type="firstName"
+                value={firstName}
+                onChange={changeFirstName}
+                required
+              />
             </Form.Group>
             <Form.Group id="signUpLastName">
               <Form.Label> Last Name </Form.Label>
-              <Form.Control type="lastName" required/>
+              <Form.Control
+                type="lastName"
+                value={lastName}
+                onChange={changeLastName}
+                required
+              />
             </Form.Group>
             <Form.Group id="signUpPhone">
               <Form.Label> Phone Number </Form.Label>
-              <Form.Control type="phone" required/>
+              <Form.Control
+                type="phone"
+                value={phone}
+                onChange={changePhone}
+                required
+              />
             </Form.Group>
             <Form.Group id="signUpAddress">
               <Form.Label> Address </Form.Label>
-              <Form.Control type="address" required/>
+              <Form.Control
+                type="address"
+                value={address}
+                onChange={changeAddress}
+                required
+              />
             </Form.Group>
             <Form.Group id="signUpEmail">
               <Form.Label> Email </Form.Label>
-              <Form.Control type="email" required/>
+              <Form.Control
+                type="email"
+                value={email}
+                onChange={changeEmail}
+                required
+              />
             </Form.Group>
             <Form.Group id="signUpPassword">
               <Form.Label> Password </Form.Label>
-              <Form.Control type="password" required/>
+              <Form.Control
+                type="password"
+                value={password}
+                onChange={changePassword}
+                required
+              />
             </Form.Group>
-            <Button className="w-100" type="submit"> Sign Up </Button>
+            <Button
+              className="w-100"
+              type="submit"
+              onSubmit={submitForm}
+            > Sign Up
+            </Button>
           </Form>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
         Already have an account? Log In
       </div>
-      {/* <h2>Create User Account</h2>
-      <form>
-        <label>
-          First Name:
-          <input
-            name="firstName"
-            type="text"
-            placeholder="Meredith"
-          />
-        </label>
-        <label>
-          Last Name:
-          <input
-            name="lastName"
-            type="text"
-            placeholder="Meredith"
-          />
-        </label>
-        <label>
-          Phone Number:
-          <input
-            name="phone"
-            type="text"
-            placeholder="Meredith"
-          />
-        </label>
-        <label>
-          Address:
-          <input
-            name="address"
-            type="text"
-            placeholder="Meredith"
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            name="email"
-            type="text"
-            placeholder="Meredith"
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            name="password"
-            type="text"
-            placeholder="Meredith"
-          />
-        </label>
-      </form> */}
     </div>
   )
 }
