b190e23b57e62bd73c7d57c7d123156aba31ee0b
liamohkay <liam.olsonkenny@gmail.com>
2021-04-03 10:57:23 -0700

    Committing for controllers from db merge

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,11 +3,7 @@ import { AuthProvider } from '../contexts/AuthContext';
 import LogIn from './LogIn';
 import SignUp from './SignUp';
 import HomePage from './Home/HomePage';
-<<<<<<< HEAD
-import VolunteerFeed from './VolunteerDashboard/Feed';
-=======
 import TileList from './TileList.jsx';
->>>>>>> dev
 
 const App = () => {
   const [userID, setUserID] = useState('');
@@ -16,13 +12,9 @@ const App = () => {
     <div id="app-container">
       <AuthProvider>
         <HomePage />
-<<<<<<< HEAD
-        <VolunteerFeed />
-=======
         <LogIn setUserID={setUserID} />
         <SignUp />
         <TileList />
->>>>>>> dev
       </AuthProvider>
    </div>
   );
