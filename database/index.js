a3884891277967fef00e04768ca5d196b321abb2
weillytong <weillydtong@gmail.com>
2021-04-03 10:27:10 -0700

    Completed getLogin and postSignUp controller and queries

--- database/index.js
+++ database/index.js
@@ -1,12 +1,56 @@
-const dbConfig = require('./dbConfig.js');
-const { Client } = require('pg');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost:27017/citizens', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const client = new Client(dbConfig);
-client.connect();
-
-client.query('SELECT 1+1 AS solution', (err, results) => {
-  if (err) throw err;
-  console.log('CONNECTED! The solution is: ', results.rows[0].solution);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('database is running')
 });
 
-module.exports = client;
+
+const usersSchema = new Schema({
+  // user_id SERIAL,
+  firebase_id: {type: String, required: true, index: true},
+  first_name: String,
+  last_name: String,
+  address: String,
+  phone_number: String,
+  email: String,
+  isVolunteer: Boolean,
+  thumbsUp: Number,
+  thumbsDown: Number,
+  photo: String,
+  tasks: [],
+})
+
+const tasksSchema = new Schema({
+  // task_id SERIAL,
+  // user_id INT,
+  volunteer_id: String,
+  requestor_id: String,
+  task_date: String,
+  task_status: String, // Pending, Accepted, Hidden, Completed
+  task_body: String,
+  task_location: String,
+  start_time: String,
+  end_time: String,
+})
+
+
+// *** Need to determine schema
+const roomsSchema = new Schema({
+  volunteer_id: String,
+  requester_id: String,
+  message_body: String,
+})
+
+const Users = mongoose.model('Users', usersSchema);
+const Tasks = mongoose.model('Tasks', tasksSchema);
+const Messages = mongoose.model('Messages', roomsSchema);
+
+module.exports = {
+  Users,
+  Tasks,
+  Messages
+}
