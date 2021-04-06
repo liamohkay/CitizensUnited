350a7a845318d9f66b97f2f3167f2a70e2b16a12
liamohkay <liam.olsonkenny@gmail.com>
2021-04-06 00:30:48 -0700

    Made dynamic route w/ taskid param and inserted maps & chat room inside

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,6 +7,7 @@ import LogIn from './LogIn';
 import SignUp from './SignUp';
 import HomePage from './Home/HomePage';
 import TileList from './Dashboard/TileList';
+import TaskView from './Dashboard/TaskView';
 import PrivateRoute from './PrivateRoute';
 import Map from './Map/Map';
 
@@ -20,6 +21,7 @@ const App = () => {
       <Router >
         <Switch>
           <PrivateRoute exact path="/" component={TileList} user={user} />
+          <PrivateRoute exact path="/:task_id" component={TaskView} user={user} />
           <Route exact path="/home" render={() => <HomePage setIsVolunteer={setIsVolunteer} />} />
           <Route exact path="/signup" render={() => <SignUp isVolunteer={isVolunteer} />} />
           <Route exact path="/login" render={() => <LogIn setUser={setUser} />} />
