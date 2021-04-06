1455de497c7c58727923a8c90a0ceb1324332484
Jordan Beaird <jordan.beaird@gmail.com>
2021-04-06 16:38:36 -0700

    fixed merge issue

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -24,13 +24,13 @@ const App = () => {
         <Switch>
           <PrivateRoute exact path="/" component={Dashboard} />
           <PrivateRoute exact path="/task/:task_id" component={TaskView} user={user} />
+          <PrivateRoute exact path="/task/rating/:task_id" component={Rating} user={user}/>
           <Route exact path="/home" render={() => <HomePage setIsVolunteer={setIsVolunteer} />} />
           <Route exact path="/signup" render={() => <SignUp isVolunteer={isVolunteer} />} />
           <Route exact path="/login" render={() => <LogIn setUser={setUser} />} />
         </Switch>
       </Router>
     </AuthProvider>
-    {/* <Rating /> */}
   </div>
   )
 }
