6d8857daf59c226b469811828ef105be79b384c8
Jordan Beaird <jordan.beaird@gmail.com>
2021-04-06 13:57:15 -0700

    working

--- client/src/components/Chat/ChatMessage.jsx
+++ client/src/components/Chat/ChatMessage.jsx
@@ -5,11 +5,11 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 const ChatMessage = (props) => {
   const { text, uid, displayName } = props.msg;
-  const msgType = uid === auth.currentUser.uid ? 'sent' : 'received';
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
   return (
     <>
-      <div className='message'>
+      <div className={`message ${messageClass}`}>
         <p>{text}</p>
       </div>
     </>)
