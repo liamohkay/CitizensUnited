b9dc3db7d1f95e5b02f37a526710bb6314ba6f88
Sarah Baek <sbaek44@gmail.com>
2021-04-06 14:49:52 -0700

    finished Rating component and after voting page is redirected to user homepage

--- client/src/components/Rating.jsx
+++ client/src/components/Rating.jsx
@@ -1,34 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
-const Rating = () => {
+const Rating = (props) => {
+  const { requestor_id, volunteer_id } = props.location.state.ticket
 
   const thumbsUp = () => {
-    axios.put('/api/ratings/thumbsUp', {firebase_id: '1KFX1nJzwbVvWXVOttsLlc5e3Tu1'})
-    .then(() => (alert('Vote Submitted')))
-    .catch((err) => (console.log(err)))
+    if (props.user.isVolunteer === true) {
+      axios.put('/api/ratings/thumbsUp', {firebase_id: requestor_id})
+      .then(() => (alert('Vote Submitted')))
+      .catch((err) => (console.log(err)))
+    } else {
+      axios.put('/api/ratings/thumbsUp', {firebase_id: volunteer_id})
+      .then(() => (alert('Vote Submitted')))
+      .catch((err) => (console.log(err)))
+    }
+    console.log('helloooo')
+    // <Link to={{pathname: "/"}}></Link>
   }
 
   const thumbsDown = () => {
-    axios.put('/api/ratings/thumbsDown',{ firebase_id:  '1KFX1nJzwbVvWXVOttsLlc5e3Tu1'})
-    .then(() => (alert('Vote Submitted')))
-    .catch((err) => (console.log(err)))
+    if (props.user.isVolunteer === true) {
+      axios.put('/api/ratings/thumbsDown', {firebase_id: requestor_id})
+      .then(() => (alert('Vote Submitted')))
+      .catch((err) => (console.log(err)))
+    } else {
+      axios.put('/api/ratings/thumbsDown', {firebase_id: volunteer_id})
+      .then(() => (alert('Vote Submitted')))
+      .catch((err) => (console.log(err)))
+    }
+    // <Link to={{pathname: "/"}}></Link>
   }
 
   return (
     <div>
-      <h2>Please Rate Your Volunteer</h2>
+      <h2>Please Rate Your {props.user.isVolunteer ? 'Requester' : 'Volunteer'}</h2>
       <div style={{display: 'flex', flexDirection: 'row'}}>
         <div>
           <h4>Thumbs Up!</h4>
-          <i onClick={thumbsUp} className="far fa-thumbs-up fa-10x"></i>
-          <p>Your volunteer was helpful and polite and you would love to have them as your volunteer again.</p>
+          <Link to={{pathname: "/"}}>
+            <i onClick={thumbsUp} className="far fa-thumbs-up fa-10x"></i>
+          </Link>
+          {props.user.isVolunteer ? <p>Your requester was polite and appreciative and you would love to volunteer for them again.</p> : <p>Your volunteer was helpful and polite and you would love to have them as your volunteer again.</p>}
         </div>
         <div>
           <h4>Thumbs Down!</h4>
-          <i onClick={thumbsDown} className="far fa-thumbs-down fa-10x"></i>
-          <p>Your volunteer was not helpful and you wish to not have them as your volunteer again.</p>
+          <Link to={{pathname: "/"}}>
+            <i onClick={thumbsDown} className="far fa-thumbs-down fa-10x"></i>
+          </Link>
+          {props.user.isVolunteer ? <p>Your requester was not polite and you wish to not volunteer for them again.</p> : <p>Your volunteer was not helpful and you wish to not have them as your volunteer again.</p>}
         </div>
       </div>
     </div>
