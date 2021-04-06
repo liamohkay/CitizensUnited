ab19f77220cde4d7b1e00b25b07809ad0635ebf9
liamohkay <liam.olsonkenny@gmail.com>
2021-04-05 23:16:36 -0700

    Chat messages rendering

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,55 +1,55 @@
+// Libraries + dependencies
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../contexts/AuthContext';
 import { chat, auth } from '../../firebase';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
+// Components
 import ChatMessage from './ChatMessage.jsx'
 
-
+// Custom hook wrapper for document data to avoid null errors
 function useDocumentDataSSR(ref, options) {
   const [value, loading, error] = useDocumentData(ref, options)
-
   if (options?.startWith && loading) {
     return [options.startWith, loading, error]
   } else {
     return [value, loading, error]
   }
-
 }
 
 const Chat = ({ room_id }) => {
-  // const { currentUser } = useAuth();
+  const { currentUser } = useAuth();
   const roomRef = chat.doc(`chatRooms/${room_id}`);
-  // const [msgStream] = useDocumentData(roomRef)
   const [msgStream] = useDocumentDataSSR(roomRef, { startWith: 'messages' })
-  // const [messages, setMessages] = useState([]);
-  const [formValue, setFormValue] = useState('');
-
-
+  const [newMsg, setNewMsg] = useState('');
 
   // Sends message to chatroom and updates firebase db
   const sendMessage = (e) => {
     e.preventDefault();
-    const { uid } = auth.currentUser;
-
+    console.log(currentUser);
     let msg = {
-      text: formValue,
+      text: newMsg,
       createdAt: new Date(),
-      uid: uid,
-      photoUrl: currentUser.photoUrl
+      uid: currentUser.uid,
+      displayName: currentUser.displayName,
+      // photoUrl: currentUser.photoUrl
     }
 
-    console.log(msg);
     roomRef.update({ messages: msgStream.messages.concat([msg]) })
-
-    setFormValue('');
+    setNewMsg('');
     // dummy.current.scrollIntoView({ behavior: 'smooth' });
   }
 
   return (
     <div className="chat-conatiner">
-
-      { JSON.stringify(msgStream?.messages) }
-
+      <div className="main">
+        {!msgStream?.messages ? null : (
+          msgStream?.messages.map(msg => <ChatMessage msg={msg} key={msg.uid + msg.text} />)
+        )}
+      </div>
+      <form className="chatForm" onSubmit={sendMessage}>
+        <input className="input" value={newMsg} onChange={(e) => setNewMsg(e.target.value)}/>
+        <button className="chatBtn" type="submit"> Send Message</button>
+      </form>
     </div>
   )
 }
--- client/src/components/Chat/ChatMessage.jsx
+++ client/src/components/Chat/ChatMessage.jsx
@@ -4,14 +4,13 @@ import { chat, auth } from '../../firebase';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 const ChatMessage = (props) => {
-  const { text, uid } = props.msg;
-  // const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  const { text, uid, displayName } = props.msg;
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
   return (
     <>
       <div className='message'>
-        {/* <img className="chatPic" src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} /> */}
-        <p>{text}</p>
+        {text}
       </div>
     </>)
 }
