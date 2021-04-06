cca43f60ccdfcec52c649205dd9869345967c18e
Sarah Baek <sbaek44@gmail.com>
2021-04-06 13:33:07 -0700

    put requests for both thumbsUp and thumbsDown working

--- client/src/components/Rating.jsx
+++ client/src/components/Rating.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const Rating = () => {
+
+  const thumbsUp = () => {
+    axios.put('/api/ratings/thumbsUp', {firebase_id: '1KFX1nJzwbVvWXVOttsLlc5e3Tu1'})
+    .then(() => (alert('Vote Submitted')))
+    .catch((err) => (console.log(err)))
+  }
+
+  const thumbsDown = () => {
+    axios.put('/api/ratings/thumbsDown',{ firebase_id:  '1KFX1nJzwbVvWXVOttsLlc5e3Tu1'})
+    .then(() => (alert('Vote Submitted')))
+    .catch((err) => (console.log(err)))
+  }
+
   return (
     <div>
       <h2>Please Rate Your Volunteer</h2>
-      <div>
-        <h4>Thumbs Up!</h4>
-        <i className="far fa-thumbs-up fa-7x"></i>
-        <p>Your volunteer was helpful and polite and you would love to have them as your volunteer again.</p>
-      </div>
-      <div>
-        <h4>Thumbs Down!</h4>
-        <i className="far fa-thumbs-down fa-7x"></i>
-        <p>Your volunteer was not helpful and you wish to not have them as your volunteer again.</p>
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+        <div>
+          <h4>Thumbs Up!</h4>
+          <i onClick={thumbsUp} className="far fa-thumbs-up fa-10x"></i>
+          <p>Your volunteer was helpful and polite and you would love to have them as your volunteer again.</p>
+        </div>
+        <div>
+          <h4>Thumbs Down!</h4>
+          <i onClick={thumbsDown} className="far fa-thumbs-down fa-10x"></i>
+          <p>Your volunteer was not helpful and you wish to not have them as your volunteer again.</p>
+        </div>
       </div>
     </div>
   )
