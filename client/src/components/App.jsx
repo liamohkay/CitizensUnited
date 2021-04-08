1238ba4182ce475b07bcc7d35ae1da54c8ab6305
liamohkay <liam.olsonkenny@gmail.com>
2021-04-07 23:09:51 -0700

    Created OldTaskViews

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,12 +2,14 @@
 import React, { useState, useEffect } from 'react';
 import { AuthProvider } from '../contexts/AuthContext';
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+
 // Components
 import LogIn from './LogIn';
 import SignUp from './SignUp';
 import HomePage from './Home/HomePage';
 import Dashboard from './Dashboard/Dashboard';
 import TaskView from './Dashboard/TaskView';
+import OldTasksView from './Dashboard/OldTasksView';
 import PrivateRoute from './PrivateRoute';
 import Map from './Map/Map';
 
@@ -25,7 +27,7 @@ const App = () => {
           <PrivateRoute exact path="/" component={Dashboard} />
           <PrivateRoute exact path="/task/:task_id" component={TaskView} user={user} />
           <PrivateRoute exact path="/task/rating/:task_id" component={Rating} user={user}/>
-          <PrivateRoute exact path="/oldtasks/:user_id" component={Rating} user={user}/>
+          <PrivateRoute exact path="/oldtasks/:user_id" component={OldTasksView} user={user}/>
           <Route exact path="/home" render={() => <HomePage setIsVolunteer={setIsVolunteer} />} />
           <Route exact path="/signup" render={() => <SignUp isVolunteer={isVolunteer} />} />
           <Route exact path="/login" render={() => <LogIn setUser={setUser} />} />
---
+++ client/src/components/Dashboard/OldTasksView.jsx
@@ -0,0 +1,12 @@
+// Libraries + dependencies
+import React from 'react';
+
+const OldTasksView = () => {
+  return (
+    <div>
+      HI
+    </div>
+  );
+};
+
+export default OldTasksView;
