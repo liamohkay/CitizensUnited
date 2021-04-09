749bd0070fef743c073e388be4e1dc59b0a45a80
liamohkay <liam.olsonkenny@gmail.com>
2021-04-09 16:38:50 -0700

    Refactored server file

--- server/index.js
+++ server/index.js
@@ -7,24 +7,23 @@ const router = require('./router.js');
 const bodyparser = require('body-parser');
 const port = 3000;
 
-// Initialize express server + apply socket io config
-const server = express();
+// Initialize express server + apply middleware
+const server = express()
+  .use(morgan('dev'))
+  .use(express.json())
+  .use(cors())
+  .use(bodyparser.json())
+  .use(bodyparser.urlencoded({ extended: true }))
+  .use('/api', router)
+
+// Set up websockets connection on server
 const ioServer = require('http').createServer(server);
 const io = require('socket.io')(ioServer)
   .of('/api/socket')
   .on('connection', socket => console.log('Socket.io: User connected'));
 
-// Apply middleware to express server
-server.use(morgan('dev'));
-server.use(express.json());
-server.use(cors());
-server.use(bodyparser.json());
-server.use(bodyparser.urlencoded({ extended: true }));
-
 // Serve up static files
 server.use(express.static(path.join(__dirname, '../client/dist/')));
 
 // Connect to router
-server.use('/api', router);
-
 ioServer.listen(port, () => console.log(`Listening on port: ${port}`));
